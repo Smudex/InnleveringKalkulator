b371beda1c2cf09fb76152ca800e2f308f6f5e31
Smudex <99404643+Smudex@users.noreply.github.com>
2022-09-09 11:39:14 +0200

    Initial Commit

---
+++ CalculatorDemo/CalculatorDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorDemoApp.swift
+//  CalculatorDemo
+//
+//  Created by Sander Hafstad on 09/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
